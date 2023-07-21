a27234457e2a195378da8e971569b393935c9e8a
Andriikryl <andreyk557@gmail.com>
2023-07-21 15:25:59 +0300

    add new scaaen

--- main.js
+++ main.js
@@ -2,32 +2,56 @@ import "./style.css";
 
 import * as THREE from "three";
 
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// camera.position.z = 5;
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   cube.rotation.x += 0.01;
+//   cube.rotation.y += 0.01;
+
+//   renderer.render(scene, camera);
+// }
+
+// animate();
+
+const canvas = document.querySelector(".web");
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const geometre = new THREE.BoxGeometry(1, 1, 1);
+const matirial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometre, matirial);
+scene.add(mesh);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const size = {
+  width: 800,
+  height: 600,
+};
 
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+camera.position.z = 3;
+scene.add(camera);
 
-function animate() {
-  requestAnimationFrame(animate);
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-}
-
-animate();
+renderer.setSize(size.width, size.height);
+renderer.render(scene, camera);
